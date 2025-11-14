6ce077f146ad75c8ce928f2ba75e7bd289e154b1
thirddelmundo-maker <third.delmundo@olivarezcollegetagaytay.edu.ph>
2025-11-14 14:06:37 +0800

    javascript-activity

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const button = document.getElementById("button");
+
+ 
+    button.addEventListener("click", function() {
+        const name = document.getElementById("name").value;
+        const age = document.getElementById("age").value; 
+        const color = document.getElementById("color").value
+
+        
+        document.getElementById("result").textContent = "Hello, " + name + " ikaw ay " + age + " anyos na " + "ang favorite color mo ay " + color + " bading!!";
+    });
+});
